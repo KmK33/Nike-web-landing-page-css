3e243268613fe94baed1d4c43f6a4e53f2fe7669
KmK33 <kmk112003@gmail.com>
2024-07-13 22:19:13 +0530

    nike-landing-page

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import PopularProduct from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProduct,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
